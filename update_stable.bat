7d057740dabcf368014913b8ed937e830c715986
PruthiviRaj27 <pruthiviraj@testpress.in>
2023-08-14 17:56:32 +0530

    Update bat file

--- update_stable.bat
+++ update_stable.bat
@@ -1,4 +1,18 @@
-git status
-git add .
-git commit -m "refcator"
-git rev-parse --short HEAD
+@echo off
+
+REM Delete the old tag locally
+git tag -d stable
+
+REM Delete the old tag remotely
+git push origin :refs/tags/stable
+
+REM Get the latest commit ID from the 'master' branch
+for /f %%i in ('git rev-parse master') do set "commitId=%%i"
+
+REM Make a new tag locally
+git tag stable %commitId%
+
+REM Push the new tag to the remote repository
+git push origin stable
+
+echo Tagging process completed.
